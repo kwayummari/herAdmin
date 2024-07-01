73511f44f6fd0c336138c8ac13753c5e330297ba
Kwayu Mmari <mmaridenis@gmail.com>
2024-07-01 18:38:12 +0300

    added table.js

--- src/screens/home/table.js
+++ src/screens/home/table.js
@@ -1,42 +1,82 @@
-import React from "react";
-import './TablesData.css'
-import { Button } from "@mui/material";
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import './TablesData.css';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function TablesData() {
-    const handleButtonClick = () => {
-        window.open("https://www.every.org/her-initiative?utm_campaign=donate-link#/donate/card");
+    const [programData, setProgramData] = useState(null);
+
+    useEffect(() => {
+        fetchProgramData();
+    }, []);
+
+    const fetchProgramData = async () => {
+        try {
+            const response = await fetch('https://herinitiative.or.tz/her-api/api/blog/get_blog.php', {
+                method: 'GET',
+            });
+            const data = await response.json();
+            setProgramData(data);
+        } catch (error) {
+            console.error('Error fetching program data:', error);
+        }
     };
-    
+    const filterSpecialCharacters = (str) => {
+        return str.replace(/[^\w\s]/gi, '');
+    };
+
+    const StyledTableCell = styled(TableCell)(({ theme }) => ({
+        [`&.${tableCellClasses.head}`]: {
+            backgroundColor: theme.palette.common.black,
+            color: theme.palette.common.white,
+        },
+        [`&.${tableCellClasses.body}`]: {
+            fontSize: 14,
+        },
+    }));
+
+    const StyledTableRow = styled(TableRow)(({ theme }) => ({
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.action.hover,
+        },
+        // hide last border
+        '&:last-child td, &:last-child th': {
+            border: 0,
+        },
+    }));
+
+
     return (
-        <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh', position: 'relative',
-            // backgroundColor: 'rgba(0, 0, 0, 0.5)'
-        }}>
-            <video autoPlay muted loop style={{
-                position: 'absolute',
-                width: '100%',
-                height: '100vh',
-                objectFit: 'cover',
-                // zIndex: '-1'
-            }}>
-                <source src="/photos/malaika.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-            <div className="overlay">
-                <div className="textContainer">
-                    {/* <p className="header1">Ensure young women have a</p> */}
-                    <p className="header2">Yes! To Financial  <br /> Freedom</p>
-                    <div style={{ display: "flex", marginTop: '40px' }}>
-                        <p className="header3">MAKE A DIFFERENCE</p>
-                        <Button variant="contained" onClick={handleButtonClick} className="button" sx={{ fontSize: '16px', fontWeight: 'bold', borderRadius: '5px' }}>
-                            DONATE NOW
-                        </Button>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>Name</StyledTableCell>
+                            <StyledTableCell align="right">Title</StyledTableCell>
+                            <StyledTableCell align="right">Description</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {programData.map((row) => (
+                            <StyledTableRow key={row.id}>
+                                <StyledTableCell align="right"><img src={`https://herinitiative.or.tz/her-api/api/blog/images/${programs.image}`} /></StyledTableCell>
+                                <StyledTableCell component="th" scope="row">
+                                {filterSpecialCharacters(row.title)}
+                                </StyledTableCell>
+                                <StyledTableCell align="right">{row.description}</StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     );
 }
